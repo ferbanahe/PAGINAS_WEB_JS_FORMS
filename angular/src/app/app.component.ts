e2f57f6a0d8b03bb186bfe9dd0e87ad769c24c79
ferbanahe <ferba6@yahoo.es>
2018-10-02 13:46:36 +0200

    Tareas Olus

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { environment } from '../environments/environment';
-
 @Component({
   selector: 'cas-root',
   templateUrl: './app.component.html',
--- angular/src/app/tareas/tareas-plus/tareas-plus.component.ts
+++ angular/src/app/tareas/tareas-plus/tareas-plus.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TareaModel, TareaIf } from '../../models/tarea.model';
 
 @Component({
   selector: 'cas-tareas-plus',
@@ -7,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TareasPlusComponent implements OnInit {
 
-  constructor() { }
+  titulo: string
+  rutaGoogle: string
+  //newTarea: TareaModel 
+  newTarea: TareaIf // POJO
+  aTareas: Array<TareaIf>
+
+  constructor() {  
+  }
 
   ngOnInit() {
+    this.titulo = 'Lista plus'
+    this.rutaGoogle = 'https://www.google.es'
+    // this.newTarea = new TareaModel('', false )
+    this.newTarea = {nombre: '', isCompleted: false}
+    this.aTareas = []
   }
+  
+  addTarea () {
+    if(!this.newTarea.nombre) {return}
+    // let tareaClon = JSON.parse( JSON.stringify(this.newTarea)) 
+    let tareaClon = Object.assign({}, this.newTarea)
+    this.aTareas.push(tareaClon)
+    console.log(this.aTareas)
+    this.newTarea.nombre = ''
+  }
+
+
+ borrarTarea(i) {
+  this.aTareas.splice(i,1)
+
+ }
+  
 
 }
